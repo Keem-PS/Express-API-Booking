0713ff0adfd31b931805d68450afcf665b4475d1
KeemPeerawit <111681277+Keem-PS@users.noreply.github.com>
2024-05-19 15:16:22 +0700

    add feature swagger for update, delete, search

--- controllers/booking.js
+++ controllers/booking.js
@@ -30,3 +30,53 @@ exports.getAllBookings = async (req, res) => {
 };
 
 
+exports.insertBooking = async (req, res) => {
+    const hotel = req.body;
+    const sql = 'INSERT INTO hotels (hotels_id, hotels_name, hotels_price, hotels_url, hotels_address, hotels_scoreReview) VALUES (?, ?, ?, ?, ?, ?)';
+    db.query(sql, [hotel.hotels_id, hotel.hotels_name, hotel.hotels_price, hotel.hotels_url, hotel.hotels_address, hotel.hotels_scoreReview ], (err, result) => {
+        if (err) {
+            res.status(500).json({ message: 'Error occurred while inserting hotel.', error: err });
+        } else {
+            res.status(201).json({ message: 'Hotel inserted successfully.' });
+        }
+    });
+};
+
+exports.updateBooking = async (req, res) => {
+    const id = Number(req.params.id);
+    const hotel = req.body;
+    const sql = 'UPDATE hotels SET hotels_name = ?, hotels_price = ?, hotels_url = ?, hotels_address = ?, hotels_scoreReview = ? WHERE hotels_id = ?';
+    db.query(sql, [hotel.hotels_name, hotel.hotels_price, hotel.hotels_url, hotel.hotels_address, hotel.hotels_scoreReview, id], (err, result) => {
+        if (err) {
+            res.status(500).json({ message: 'Error occurred while updating hotel.', error: err });
+        } else {
+            res.status(200).json({ message: 'Hotel updated successfully.' });
+        }
+    });
+};
+
+exports.deleteBooking = async (req, res) => {
+    const id = Number(req.params.id);
+    const sql = 'DELETE FROM hotels WHERE hotels_id = ?';
+    db.query(sql, [id], (err, result) => {
+        if (err) {
+            res.status(500).json({ message: 'Error occurred while deleting hotel.', error: err });
+        } else {
+            res.status(200).json({ message: 'Hotel deleted successfully.' });
+        }
+    });
+};
+
+exports.searchBookings = async (req, res) => {
+    const keyword = req.params.keyword;
+    const sql = 'SELECT * FROM hotels WHERE hotels_name LIKE ?';
+    db.query(sql, [`%${keyword}%`], (err, result) => {
+        if (err) {
+            res.status(500).json({ message: 'Error occurred while retrieving hotels.', error: err });
+        } else {
+            res.status(200).json(result);
+        }
+    });
+};
+
+
--- routers/booking.js
+++ routers/booking.js
@@ -1,8 +1,42 @@
 const express = require('express');
 const router = express.Router();
-const { getBookingById, getAllBookings } = require('../controllers/booking');
+const { getBookingById, getAllBookings, insertBooking, updateBooking, deleteBooking, searchBookings } = require('../controllers/booking');
 
-
+/**
+ * @swagger
+ * /api/booking/{id}:
+ *   get:
+ *     summary: Retrieve a single hotel by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: The hotel ID
+ *     responses:
+ *       200:
+ *         description: A single hotel
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 hotels_id:
+ *                   type: integer
+ *                 hotels_name:
+ *                   type: string
+ *                 hotels_price:
+ *                   type: number
+ *                 hotels_url:
+ *                   type: string
+ *                 hotels_address:
+ *                   type: string
+ *                 hotels_scoreReview:
+ *                   type: number
+ *       404:
+ *         description: Hotel not found
+ */
 
 router.get('/:id', getBookingById);
 
@@ -13,7 +47,168 @@ router.get('/:id', getBookingById);
  *     summary: Get all hotels
  *     responses:
  *       200:
- *         description: A list of hotels
+ *         description: A single hotel
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 hotels_id:
+ *                   type: integer
+ *                 hotels_name:
+ *                   type: string
+ *                 hotels_price:
+ *                   type: number
+ *                 hotels_url:
+ *                   type: string
+ *                 hotels_address:
+ *                   type: string
+ *                 hotels_scoreReview:
+ *                   type: number
+ *       404:
+ *         description: Hotel not found
+ */
+
+router.get('/', getAllBookings);
+
+/**
+ * @swagger
+ * /api/booking/:
+ *   post:
+ *     summary: Create a new hotel
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *                 hotels_id:
+ *                   type: integer
+ *                 hotels_name:
+ *                   type: string
+ *                 hotels_price:
+ *                   type: number
+ *                 hotels_url:
+ *                   type: string
+ *                 hotels_address:
+ *                   type: string
+ *                 hotels_scoreReview:
+ *                   type: number
+ *     responses:
+ *       201:
+ *         description: The created hotel
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 hotels_id:
+ *                   type: integer
+ *                 hotels_name:
+ *                   type: string
+ *                 hotels_price:
+ *                   type: number
+ *                 hotels_url:
+ *                   type: string
+ *                 hotels_address:
+ *                   type: string
+ *                 hotels_scoreReview:
+ *                   type: number
+ */
+router.post('/', insertBooking);
+
+/**
+ * @swagger
+ * /api/booking/{id}:
+ *   put:
+ *     summary: Update a hotel by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: The hotel ID
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *                 hotels_id:
+ *                   type: integer
+ *                 hotels_name:
+ *                   type: string
+ *                 hotels_price:
+ *                   type: number
+ *                 hotels_url:
+ *                   type: string
+ *                 hotels_address:
+ *                   type: string
+ *                 hotels_scoreReview:
+ *                   type: number
+ *     responses:
+ *       200:
+ *         description: The updated hotel
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 hotels_id:
+ *                   type: integer
+ *                 hotels_name:
+ *                   type: string
+ *                 hotels_price:
+ *                   type: number
+ *                 hotels_url:
+ *                   type: string
+ *                 hotels_address:
+ *                   type: string
+ *                 hotels_scoreReview:
+ *                   type: number
+ *       404:
+ *         description: Hotel not found
+ */
+router.put('/:id', updateBooking);
+
+/**
+ * @swagger
+ * /api/booking/{id}:
+ *   delete:
+ *     summary: Delete a hotel by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: The hotel ID
+ *     responses:
+ *       204:
+ *         description: No content
+ *       404:
+ *         description: Hotel not found
+ */
+router.delete('/:id', deleteBooking);
+
+/**
+ * @swagger
+ * /api/booking/search/{keyword}:
+ *   get:
+ *     summary: Search hotels by keyword
+ *     parameters:
+ *       - in: path
+ *         name: keyword
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The keyword to search for in hotel names
+ *     responses:
+ *       200:
+ *         description: A list of matching hotels
  *         content:
  *           application/json:
  *             schema:
@@ -21,16 +216,21 @@ router.get('/:id', getBookingById);
  *               items:
  *                 type: object
  *                 properties:
- *                   id:
- *                     type: integer
- *                   name:
- *                     type: string
+ *                   hotels_id:
+ *                      type: integer
+ *                   hotels_name:
+ *                      type: string
+ *                   hotels_price:
+ *                      type: number
+ *                   hotels_url:
+ *                      type: string
+ *                   hotels_address:
+ *                      type: string
+ *                   hotels_scoreReview:
+ *                      type: number
+ *       500:
+ *         description: Error occurred while retrieving hotels
  */
-
-router.get('/', getAllBookings);
-// router.post('/', insertBooking);
-// router.put('/:id', updateBooking);
-// router.delete('/:id', deleteBooking);
-// router.get('/search/:keyword', searchBookings);
+router.get('/search/:keyword', searchBookings);
 
 module.exports = router;
